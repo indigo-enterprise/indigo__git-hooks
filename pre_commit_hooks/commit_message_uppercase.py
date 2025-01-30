5602e5cdbfec75e8462533d01ae5f16553184d14
droldan-indigo <droldan@indigo.tech>
2025-01-30 17:20:55 -0500

    - id: commit-message-uppercase

--- pre_commit_hooks/commit_message_uppercase.py
+++ pre_commit_hooks/commit_message_uppercase.py
@@ -1,21 +1,38 @@
 import sys
 
-def check_commit_message(commit_msg_file):
-    """Verifica si el mensaje de commit comienza con mayúscula."""
+def main():
+    commit_msg_file = sys.argv[1]
+
     with open(commit_msg_file, "r", encoding="utf-8") as f:
-        first_line = f.readline().strip()
+        commit_msg = f.readline().strip()
 
-    if not first_line:
-        print("❌ Error: El mensaje de commit está vacío.")
-        return 1  # Retorna error si el mensaje está vacío.
+    if not commit_msg or not commit_msg[0].isupper():
+        print("❌ El mensaje de commit debe empezar con una letra mayúscula.")
+        return 1
 
-    if not first_line[0].isupper():
-        print(f"❌ Error: El mensaje de commit debe comenzar con mayúscula. 📝 '{first_line}'")
-        return 1  # Retorna error si no empieza con mayúscula.
-
-    print("✅ Commit message válido.")
-    return 0  # Éxito
+    return 0
 
 if __name__ == "__main__":
-    commit_msg_file = sys.argv[1]  # El archivo con el mensaje del commit
-    sys.exit(check_commit_message(commit_msg_file))
+    sys.exit(main())
+    
+# import sys
+
+# def check_commit_message(commit_msg_file):
+#     """Verifica si el mensaje de commit comienza con mayúscula."""
+#     with open(commit_msg_file, "r", encoding="utf-8") as f:
+#         first_line = f.readline().strip()
+
+#     if not first_line:
+#         print("❌ Error: El mensaje de commit está vacío.")
+#         return 1  # Retorna error si el mensaje está vacío.
+
+#     if not first_line[0].isupper():
+#         print(f"❌ Error: El mensaje de commit debe comenzar con mayúscula. 📝 '{first_line}'")
+#         return 1  # Retorna error si no empieza con mayúscula.
+
+#     print("✅ Commit message válido.")
+#     return 0  # Éxito
+
+# if __name__ == "__main__":
+#     commit_msg_file = sys.argv[1]  # El archivo con el mensaje del commit
+#     sys.exit(check_commit_message(commit_msg_file))
