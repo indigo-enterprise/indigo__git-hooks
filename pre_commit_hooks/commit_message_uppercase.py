886e9fac830197194951858aa5e1f2bb4095a4a8
droldan-indigo <droldan@indigo.tech>
2025-01-30 22:26:43 -0500

    change

--- pre_commit_hooks/commit_message_uppercase.py
+++ pre_commit_hooks/commit_message_uppercase.py
@@ -1,5 +1,5 @@
-# import subprocess
-# import sys
+import subprocess
+import sys
 
 # def get_commit_message():
 #     """Obtiene el mensaje del último commit."""
@@ -14,27 +14,65 @@
 #     except subprocess.CalledProcessError:
 #         print("Error al obtener el mensaje de commit.")
 #         sys.exit(1)
+def get_commit_message():
+    """Obtiene el mensaje de commit usando `git commit -m "mensaje"`."""
+    try:
+        result = subprocess.run(
+            ["git", "config", "--get", "core.editor"],
+            capture_output=True,
+            text=True,
+            check=True
+        )
+        if result.stdout.strip():
+            print("🔹 Usando editor de commits, validación no se ejecutará.")
+            return None
 
-import sys
+        # Si el usuario usa `git commit -m "mensaje"`
+        result = subprocess.run(
+            ["git", "log", "--format=%B", "-n", "1", "HEAD"],
+            capture_output=True,
+            text=True,
+            check=True
+        )
+        return result.stdout.strip()
+    except subprocess.CalledProcessError:
+        return None
 
 def main():
-    print("main")
-    commit_msg_file = sys.argv[1]
-    
-    with open(commit_msg_file, "r", encoding="utf-8") as f:
-        commit_msg = f.readline().strip()
-
-    print(f"{commit_msg_file} --> commit_msg_file")
-    print(f"{commit_msg} --> mensaje")
-
+    commit_msg = get_commit_message()
+    print(f"{commit_msg} -- commit_msg")
     if not commit_msg or not commit_msg[0].isupper():
-        print("El mensaje de commit debe empezar con una letra mayuscula.")
-        return 1
+        print("El mensaje de commit debe empezar con una letra mayúscula.")
+        print("Usa `git commit --amend` para corregir el mensaje.")
+        sys.exit(1)
 
-    return 0
+    sys.exit(0)
 
 if __name__ == "__main__":
-    sys.exit(main())
+    main()
+
+
+# import sys
+
+# def main():
+#     commit_msg_file = sys.argv[1]
+    
+#     with open(commit_msg_file, "r", encoding="utf-8") as f:
+#         commit_msg = f.readline().strip()
+
+#     print(f"{commit_msg_file} --> commit_msg_file")
+#     print(f"{commit_msg} --> mensaje")
+#     if not commit_msg or not commit_msg[0].isupper():
+#         print("El mensaje de commit debe empezar con una letra mayuscula.")
+#         return 1
+
+#     return 0
+
+# if __name__ == "__main__":
+#     sys.exit(main())
+
+
+
 
 
 # import sys
